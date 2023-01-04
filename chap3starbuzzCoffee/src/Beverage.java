a7e7475feae005f6bc669dd6749f31245f6f1bc2
Jin Zhang <Jin.Zhang@g20strategies.com>
2023-01-04 21:07:13 +0100

    chap4 java.io uses decorator pattern

--- chap3starbuzzCoffee/src/Beverage.java
+++ chap3starbuzzCoffee/src/Beverage.java
@@ -1,8 +1,18 @@
 public abstract class Beverage {
     String description = "Unknown Beverage";
+    int size = 0;
 
     public String getDescription() {
         return description;
     }
     public abstract double cost();
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
--- chap3starbuzzCoffee/src/CondimentDecorator.java
+++ chap3starbuzzCoffee/src/CondimentDecorator.java
@@ -1,4 +1,5 @@
 public abstract class CondimentDecorator extends Beverage {
     @Override
     public abstract String getDescription();
+
 }
