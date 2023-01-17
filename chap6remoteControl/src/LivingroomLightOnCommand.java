046753ca9ccc74186f2e5838f8309e2208f01167
Jin <frankjinzhang@gmail.com>
2023-01-17 11:38:54 +0800

    added remoteControl

---
+++ chap6remoteControl/src/LivingroomLightOnCommand.java
@@ -0,0 +1,12 @@
+
+public class LivingroomLightOnCommand implements Command {
+	Light light;
+
+	public LivingroomLightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	public void execute() {
+		light.on();
+	}
+}
